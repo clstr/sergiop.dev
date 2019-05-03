febc93f942fb25800ce8d034441c621f78581894
Sergio Palomino <serg2k9@AlphaOmega>
2019-05-02 21:48:24 -0400

    404 page

---
+++ src/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class PageNotFound extends Component {
+  render() {
+    return (
+      <React.Fragment>
+         <h1>Aw shit, here we go again.</h1>
+      </React.Fragment>
+    );
+  }
+}
